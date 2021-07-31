82cbde8dad3b3c04650a42525db3a47e3aa19949
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-31 12:02:28 -0500

    created function for equals button and tested console log

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,13 @@ $(document).ready(onReady);
 function onReady() {
     console.log('so ready!');
 
+ ///// console.log('equalsBtn', $('equalsBtn'));
+    // run a function when = button is pressed
+    $('#equalsBtn').on('click', calculateEquation);
+}
+
+function calculateEquation() {
+    console.log('inside calculateEquation');
+    
+    
 }
